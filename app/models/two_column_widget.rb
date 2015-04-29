22e89ce4f5a6cf9a71e1180ecf7171da6d4c5ee4
Karina <kt@jetaido.com>
2015-04-29 15:12:22 +0200

    update for scrivito sdk 0.60

--- app/models/two_column_widget.rb
+++ app/models/two_column_widget.rb
@@ -1,4 +1,14 @@
 class TwoColumnWidget < Widget
+  attribute :column_1, :widgetlist
+  attribute :column_1_width, :string
+  attribute :column_2, :widgetlist
+  attribute :column_2_width, :string
+
+  attribute :column_1, :widgetlist
+  attribute :column_2, :widgetlist
+  attribute :column_1_width, :string
+  attribute :column_2_width, :string
+
   def column_1_width
     self[:column_1_width].presence || '6'
   end
@@ -6,4 +16,5 @@ class TwoColumnWidget < Widget
   def column_2_width
     self[:column_2_width].presence || '6'
   end
+
 end
