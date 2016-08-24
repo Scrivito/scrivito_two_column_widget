568bff312d49688782c32a91a92d32b1ba8984e1
Gert Geidel <gert.geidel@infopark.de>
2016-08-24 18:31:26 +0200

    - Locals for thumbnail and details views - Keep columns on mobile attribute - Padding between columns adjustable

--- README.md
+++ README.md
@@ -15,3 +15,19 @@ Add this line to your application's Gemfile:
 ## Customization
 
 We recommend to use [Scrivito Advanced Editors](https://github.com/Scrivito/scrivito_advanced_editors) which provides nicer controls.
+
+## Localization
+
+The following code represents the default localization for English. Copy it to your `en.yml` and change it if necessary:
+
+```yaml
+en:
+  scrivito_two_column_widget:
+    thumbnail:
+      title: Two Columns
+      description: Two columns that can be filled with widgets
+    details:
+      column_1_width: Width for column 1
+      grid_size: Gird Size
+      keep_columns: Show columns on mobile
+```
--- app/models/two_column_widget.rb
+++ app/models/two_column_widget.rb
@@ -2,9 +2,23 @@ class TwoColumnWidget < Widget
   attribute :column_1, :widgetlist
   attribute :column_2, :widgetlist
   attribute :column_1_width, :enum, values: (1..12).to_a.map {|e| e.to_s}, default: '6'
+  attribute :grid_size, :enum, values: ['small','default','large'], default: 'default'
+  attribute :keep_columns, :enum, values: ['Yes','No'], default: 'No'
 
   def column_2_width
+    val = column_2_width_value
+    val < 1 ? 1 : val
+  end
+
+  def column_2_width_value
     12 - self.column_1_width.to_i
   end
 
+  def grid_size_class
+    "#{grid_size || 'default'}-grid"
+  end
+
+  def device_class
+    keep_columns == 'Yes' ? 'xs' : 'md'
+  end
 end
